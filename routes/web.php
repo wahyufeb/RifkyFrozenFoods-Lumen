4272c1cfab539dd6f6ba1cc5f62faf34a54da17e
Wahyu Febrianto <febriantowahyu63@gmail.com>
2020-09-15 01:37:47 +0700

    Create Product Storage routes, and Fix Another routes

--- routes/web.php
+++ routes/web.php
@@ -25,14 +25,17 @@ $router->group(['prefix' => 'api'], function () use ($router) {
 	| Berisi fitur untuk mengatur Product
 	*/
 	$router->group(['prefix' => 'product-category'], function () use ($router) {
-		// Get All
+		// Ambil seluruh data kategori produk
 		$router->get('/', 'ProductCategories\ProductCategoryController@productCategories');
-		// Delete
-		$router->delete('{dataId}/delete', 'ProductCategories\ProductCategoryController@delete');
-		// Update
-		$router->put('{dataId}/update', 'ProductCategories\ProductCategoryController@update');
-		// Create 
+
+		// Menghapus kategori produk
+		$router->delete('{productCategoryId}/delete', 'ProductCategories\ProductCategoryController@delete');
+
+		// Membuat kategori produk baru 
 		$router->post('/create', 'ProductCategories\ProductCategoryController@save');
+
+		// Update
+		// $router->put('{productCategoryId}/update', 'ProductCategories\ProductCategoryController@update');
 	});
 
 	/*
@@ -59,12 +62,15 @@ $router->group(['prefix' => 'api'], function () use ($router) {
 	| Berisi fitur untuk mengatur Kios
 	*/
 	$router->group(['prefix' => 'stores'], function () use ($router) {
-		// Get All
+		// Ambil seluruh data kios dari stores
 		$router->get('/', 'Stores\StoreController@stores');
+
 		// Delete
 		$router->delete('{dataId}/delete', 'Stores\StoreController@delete');
+
 		// Update
 		$router->put('{dataId}/update', 'Stores\StoreController@update');
+
 		// Create 
 		$router->post('/create', 'Stores\StoreController@save');
 	});
@@ -76,15 +82,19 @@ $router->group(['prefix' => 'api'], function () use ($router) {
 	| Berisi fitur untuk mengatur Produk
 	*/
 	$router->group(['prefix' => 'products'], function () use ($router) {
-		// Get All
+		// Ambil seluruh data produk
 		$router->get('/', 'Products\ProductController@products');
-		// Total Products
+
+		// Hitung jumlah produk yang ada
 		$router->get('/total-product', 'Products\ProductController@totalProducts');
-		// Delete
-		$router->delete('{dataId}/delete', 'Products\ProductController@delete');
-		// Update
-		$router->post('{dataId}/update', 'Products\ProductController@update');
-		// Create 
+
+		// Menghapus produk
+		$router->delete('{productId}/delete', 'Products\ProductController@delete');
+
+		// Mengedit produk
+		$router->post('{productId}/update', 'Products\ProductController@update');
+
+		// Membuat produk baru 
 		$router->post('/create', 'Products\ProductController@save');
 	});
 
@@ -95,17 +105,101 @@ $router->group(['prefix' => 'api'], function () use ($router) {
 	| Berisi fitur untuk mengatur Fakur
 	*/
 	$router->group(['prefix' => 'invoice'], function () use ($router) {
-		// Get All
+		// Ambil data invoice dari seluruh store
 		$router->get('/', 'Invoices\InvoiceController@invoices');
-		// Get Invoices Today
-		$router->get('/today', 'Invoices\InvoiceController@invoicesToday');
-		//  Get Invoices by Date from and Date to
+
+		// Ambil data invoice diantara dateFrom dan dateTo berdasarkan storeId
+		$router->get('/{storeId}/{dateFrom}/{dateTo}', 'Invoices\InvoiceController@invoicesDateByStore');
+
+		//  Ambil data invoice diantara dateFrom dan dateTo
 		$router->get('/{dateFrom}/{dateTo}', 'Invoices\InvoiceController@invoicesDate');
+
+		// Jumlahkan seluruh kolom total pada invoice hari ini
+		$router->get('/total-income-today', 'Invoices\InvoiceController@invoiceIncomeToday');
+
+		// Jumlah seluruh kolom total pada invoice
+		$router->get('/total-income', 'Invoices\InvoiceController@invoiceIncomeAll');
+
+		// Kasir membuat transaksi dengan membuat invoice dan transaction
+		$router->post('/create', 'Invoices\InvoiceController@save');
+
+		// Ambil data invoice hari ini berdasarkan storeId
+		$router->get('{storeId}/today', 'Invoices\InvoiceController@invoiceTodayByStoreId');
+
+		// Ambil data dan jumlahkan kolom total pada invoice hari ini berdasarkan storeId
+		$router->get('{storeId}/total-income-today', 'Invoices\InvoiceController@invoiceIncomeTodayByStore');
+
 		// Delete
 		// $router->delete('{dataId}/delete', 'Invoices\InvoiceController@delete');
+
 		// Update
 		// $router->post('{dataId}/update', 'Invoices\InvoiceController@update');
-		// Create 
-		$router->post('/create', 'Invoices\InvoiceController@save');
+
+		// Get Invoices Today
+		// $router->get('/today', 'Invoices\InvoiceController@invoicesToday');
+
+	});
+
+	/*
+	|--------------------------------------------------------------------------
+	| Cashier Router
+	|--------------------------------------------------------------------------
+	| Berisi fitur untuk mengatur Kasir
+	*/
+	$router->group(['prefix' => 'users/cashier'], function () use ($router) {
+		// Ambil seluruh data cashier
+		$router->get('/', 'Cashier\CashierController@allCashier');
+
+		// Menghapus cashier
+		$router->delete('{cashierId}/delete', 'Cashier\CashierController@delete');
+
+		// Mengedit name dan password cashier
+		$router->put('{cashierId}/update', 'Cashier\CashierController@update');
+
+		// Membuat cashier baru 
+		$router->post('/create', 'Cashier\CashierController@save');
+	});
+
+	/*
+	|--------------------------------------------------------------------------
+	| Warehouse Admin Router
+	|--------------------------------------------------------------------------
+	| Berisi fitur untuk mengatur Admin Gudang
+	*/
+	$router->group(['prefix' => 'users/warehouse'], function () use ($router) {
+		// Ambil seluruh data warehouse admin
+		$router->get('/', 'WarehouseAdmin\WarehouseAdminController@allWarehouseAdmin');
+
+		// Menghapus warehouse admin
+		$router->delete('{warehouseId}/delete', 'WarehouseAdmin\WarehouseAdminController@delete');
+
+		// Mengedit warehouse admin
+		$router->put('{warehouseId}/update', 'WarehouseAdmin\WarehouseAdminController@update');
+
+		// Membuat warehouse admin baru 
+		$router->post('/create', 'WarehouseAdmin\WarehouseAdminController@save');
+	});
+
+	/*
+	|--------------------------------------------------------------------------
+	| Product Storage Router
+	|--------------------------------------------------------------------------
+	| Berisi fitur untuk mengatur Penyimpanan Produk
+	*/
+	$router->group(['prefix' => 'product-storage'], function () use ($router) {
+		// Ambil data produk dari product storage berdasarkan id_store
+		$router->get('{storeId}', 'ProductStorage\ProductStorageController@getByStoreId');
+
+		// Menambah stok / data jika belum ada dan mengupdate jika sudah ada pada products_storage
+		$router->post('incoming-goods', 'ProductStorage\ProductStorageController@incomingGoods');
+
+		// Mengupdate data dengan mengurangi stok pada products_storage
+		$router->post('exit-item', 'ProductStorage\ProductStorageController@exitItem');
+
+		// Hitung jumlah produk yang ada dari product-storage berdasarkan id_store
+		$router->get('{storeId}/total-product', 'ProductStorage\ProductStorageController@totalProductByStoreId');
+
+		// Hitung jumlah stok produk dari product-storage berdasarkan id_store
+		$router->get('{storeId}/total-stock', 'ProductStorage\ProductStorageController@totalStockByStoreId');
 	});
 });
